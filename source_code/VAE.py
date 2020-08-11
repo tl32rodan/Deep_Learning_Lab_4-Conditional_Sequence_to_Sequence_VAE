5d36d2ccda427130a2e90de98a41270b5df92810
f1b7a489-717f-441d-b83b-054fb76a0f61 <tl32rodan.cs09g@nctu.edu.tw>
2020-08-11 19:54:09 +0800

    Finish VAE

--- source_code/VAE.py
+++ source_code/VAE.py
@@ -6,6 +6,9 @@ import matplotlib.pyplot as plt
 plt.switch_backend('agg')
 import matplotlib.ticker as ticker
 import numpy as np
+import random
+
+MAX_PRED_LENGTH = 25
 
 
 #Encoder
@@ -22,13 +25,7 @@ class EncoderRNN(nn.Module):
         output = embedded
         output, hidden = self.lstm(output, hidden)
         
-        #---------- Todo: Add (2) fully connected layers ------------#
-        
         return output, hidden
-
-    def initHidden(self):
-        return torch.zeros(1, 1, self.hidden_size, device=device)
-
 
 
 #Decoder
@@ -39,24 +36,96 @@ class DecoderRNN(nn.Module):
 
         self.embedding = nn.Embedding(output_size, hidden_size)
         self.lstm = nn.LSTM(hidden_size, hidden_size)
-        self.out = nn.Linear(hidden_size, output_size)
-        self.softmax = nn.LogSoftmax(dim=1)
-
+        
     def forward(self, input, hidden):
         output = self.embedding(input).view(1, 1, -1)
-        output = F.relu(output) ########## Is this necessary?
+        
         output, hidden = self.lstm(output, hidden)
-        output = self.out(output[0])
         return output, hidden
 
-    def initHidden(self):
-        return torch.zeros(1, 1, self.hidden_size, device=device)
+
+# VAE model
+class VAE(nn.Module):
+    def __init__(self,input_size, hidden_size, output_size, teacher_forcing_ratio = 1.0):
+        super(VAE, self).__init__()
+
+        self.encoder   = EncoderRNN(input_size, hidden_size)
+        self.fc_mu     = nn.Linear(hidden_size, hidden_size)
+        self.fc_logvar = nn.Linear(hidden_size, hidden_size)
+        self.decoder   = DecoderRNN(hidden_size, output_size)
+        self.teacher_forcing_ratio = teacher_forcing_ratio
+        
+        #The number of vocabulary
+        self.vocab_size = 28
+        self.SOS_token = 0
+        self.EOS_token = self.vocab_size-1
+        
+    def encode(self, input_seq, hidden):
+        for c in input_seq:
+            input_index = torch.LongTensor([[c]])
+            _, hidden = self.encoder(input_index, hidden)
+        
+        mu     = self.fc_mu(hidden)
+        logvar = self.fc_logvar(hidden)
+        
+        return mu, logvar
+
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5*logvar)
+        
+        # eps is generated from N(0,I)
+        eps = torch.randn_like(std)
+        
+        return mu + eps*std
+
+    def decode(self, hidden, use_teacher_forcing = False, target_tensor = None):
+        result = []
+               
+        # Set decoder_input as SOS
+        decoder_input = decoder_input = torch.tensor([[self.SOS_token]])
+        
+        if use_teacher_forcing:
+            # target_tensor should not be None
+            assert target_tensor is not None, "target_tensor should be specified"
+            
+            # Teacher forcing: Feed the target as the next input
+            for i in range(len(target_tensor)):
+                decoder_output, hidden = self.decoder(decoder_input, hidden)
+                topv, topi = decoder_output.topk(1)
+                result.append(topi.squeeze().detach())
+                
+                decoder_input = target_tensor[i]  # Teacher forcing
+
+        else:
+            # Without teacher forcing: use its own predictions as the next input
+            for i in range(MAX_PRED_LENGTH):
+                decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden)
+                topv, topi = decoder_output.topk(1)                
+                result.append(topi.squeeze().detach())
+                
+                decoder_input = topi.squeeze().detach()  # detach from history as input
+
+                if decoder_input.item() == self.EOS_token: 
+                    break
     
+        return result
+
+    def forward(self, x, hidden, use_teacher_forcing = False, target_tensor = None):
+        # Encode
+        mu, logvar = self.encode(x, hidden)
+        
+        # Reparameterize
+        hidden = self.reparameterize(mu, logvar)
+        
+        result = self.decode(hidden, use_teacher_forcing, target_tensor)
+        
+        return result, mu, logvar
+
 
 # Reference: https://github.com/pytorch/examples/blob/master/vae
 # Reconstruction + KL divergence losses summed over all elements and batch
-def loss_function(recon_x, x, mu, logvar):
-    CE = F.cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
+def VAE_Loss(recon_x, x, mu, logvar):
+    BCE = F.binary_cross_entropy(recon_x, x, reduction='sum')
 
     # see Appendix B from VAE paper:
     # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
@@ -64,5 +133,5 @@ def loss_function(recon_x, x, mu, logvar):
     # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
     KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
 
-    return CE + KLD
+    return BCE + KLD
 
--- source_code/dataloader.py
+++ source_code/dataloader.py
@@ -1,16 +1,9 @@
-def load_data():
-    train_vocab = []
-    test_vocab = []
-    with open('data/train.txt') as f:
+def load_data(filename):
+    vocab = []
+    with open(filename) as f:
         for line in iter(f):
             line = line[:-1].split(' ')
-            train_vocab.append(line)
+            vocab.append(line)
         f.close()
     
-    with open('data/test.txt') as f:
-        for line in iter(f):
-            line = line[:-1].split(' ')
-            test_vocab.append(line)
-        f.close()
-
-    return train_vocab, test_vocab
+    return vocab
